75c4cf752697ce07a9d8906ca484190ec36d1e7e
Lisa Reiners <lisatroconis26@gmail.com>
2023-08-07 16:59:56 +0200

    Modify the entry of the uuid and the Pii section

--- pii.R
+++ pii.R
@@ -4,60 +4,68 @@ library(readxl)
 file <- "C:/Users/lisa.reiners/OneDrive - ACTED/Documents/IRQ2301/review_template/inputs/IRQ2301_Winter_cash_assistance_dataset.xlsx"
 data <- read_excel(file, guess = 10000, sheet = "Clean Data")
 
-#Check PII
-names_col <- colnames(data)
-pii_col <- character(0)
-
-for (name in tolower(names_col)){
-  contains_substring <- grepl("name|gps|phone|location|lat|long", name)
-  if(contains_substring == TRUE){
-    pii_col <- append(pii_col, name)
-  }
-}
-
-# Show only the columns identified as PII
-pii_df <- cleann[, pii_col]
-
-# # Drop columns that are empty
-# pii_df <- pii_df[, colSums(is.na(pii_df)) < nrow(pii_df)]
-# 
-# # Get the unique values for each column
-# unique_matrix <- apply(pii_df, 2, unique)
-
-
-#function (data, i.know.this.check.is.insufficient = F) 
-#{
-  #Check PII
+function (data, i.know.this.check.is.insufficient = F) 
+ {
+  # Check for sensitive columns
   names_col <- colnames(data)
-  pii_col <- character(0)
+  sensitive.cols <- character(0)
   
-  for (name in tolower(names_col)){
-      contains_substring <- grepl("name|gps|phone|location|lat|long", name)
-      if(contains_substring == TRUE){
-        pii_col <- append(pii_col, name)
+  for (name in tolower(names_col)) {
+    contains_substring <- grepl("name|gps|phone|location|latitude|longitude", name)
+    if (contains_substring) {
+      sensitive.cols <- append(sensitive.cols, name)
+    }
+  }
+  
+  if (length(sensitive.cols) == 0) {
+    # You need to define what empty_issues_table() does or remove this line if not needed.
+    return(empty_issues_table())
+  }
+  
+  # Show only the columns identified as PII
+  pii_df <- data[, sensitive.cols]
+  
+  # Drop columns that are empty
+  pii_df <- pii_df[, colSums(is.na(pii_df)) < nrow(pii_df)]
+  
+  # Get the unique values for each column
+  unique_matrix <- apply(pii_df, 2, unique)
+  
+  # Create a dataframe of the unique_matrix
+  unique_df <- data.frame(
+    variable = names(unique_matrix),
+    unique_values = I(unique_matrix),
+    stringsAsFactors = FALSE
+  )
+  
+  
+  # Create the unique_sensitive.cols dataframe
+  sensitive.cols <- data.frame(
+    index = NA,
+    value = sapply(unique_matrix, function(values) {
+      if (length(values) >= 3) {
+        return(paste(head(values, 3), collapse = ", ") %>% paste0("..."))
+      } else {
+        return(paste(values, collapse = ", "))
       }
-  }
+    }),
+    variable = unique_df$variable,
+    has_issue = TRUE,
+    issue_type = "Potentially sensitive information. Please ensure all PII is removed",
+    stringsAsFactors = FALSE
+  )
   
-  for (name in tolower(names_col)){
-    sensitive.cols <- grepl("name|gps|phone|location|lat|long", 
-                           x = name)
-    if(sensitive.cols == TRUE){
-    pii_col <- append(sensitive.cols, name)
-    }
-  }
+  # Reset row names
+  rownames(sensitive.cols) <- NULL
   
-  
-    if (length(sensitive.cols) == 0) {
-      return(empty_issues_table())
-    }
-  
-  
-  sensitive.cols <- data.frame(index = NA, value = NA, variable = sensitive.cols, 
-                               has_issue = TRUE, issue_type = "Potentially sensitive information. Please ensure all PII is removed", 
-                               stringsAsFactors = F)
   if (!i.know.this.check.is.insufficient) {
     warning("sensitive_columns() is rudimentary and does not provide ANY data protection.")
   }
   return(sensitive.cols)
+}
+
+
+
+
 
 
